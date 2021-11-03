87c62265f84a5cb15b9d782e69c0a38d7565df20
annaKulyk <ankakulik1998@gmail.com>
2021-11-03 08:32:42 +0200

    added weeks navigation, open/close Modal logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,68 @@
 import React, { useState } from 'react';
+import moment from 'moment';
 import Header from './components/header/Header.jsx';
 import Calendar from './components/calendar/Calendar.jsx';
+import Modal from './components/modal/Modal.jsx';
 
 import { getWeekStartDate, generateWeekRange } from './utils/dateUtils.js';
 
 import './common.scss';
 
 const App = () => {
-  const [state] = useState({
-    weekStartDate: new Date(),
+  const [weekStart, setWeekStart] = useState({
+    weekStartDate: moment().toDate(),
+  });
+  const [modalStatus, setModalStatus] = useState({
+    showModal: false,
   });
 
-  const { weekStartDate } = state;
+  const { weekStartDate } = weekStart;
+  const { showModal } = modalStatus;
+
+  const goNextWeek = () => {
+    const nextWeekStart = moment(weekStartDate).add(7, 'days');
+    setWeekStart({
+      weekStartDate: nextWeekStart.toDate(),
+    });
+  };
+
+  const goPrevWeek = () => {
+    const nextWeekStart = moment(weekStartDate).subtract(7, 'days');
+    setWeekStart({
+      weekStartDate: nextWeekStart.toDate(),
+    });
+  };
+
+  const goCurrentWeek = () => {
+    setWeekStart({
+      weekStartDate: moment().toDate(),
+    });
+  };
+
+  const showModalHandler = () => {
+    setModalStatus({
+      showModal: true,
+    });
+  };
+
+  const hideModalHandler = () => {
+    setModalStatus({
+      showModal: false,
+    });
+  };
+
   const weekDates = generateWeekRange(getWeekStartDate(weekStartDate));
 
   return (
     <>
-      <Header />
+      <Header
+        weekStartDate={weekStartDate}
+        goNextWeek={goNextWeek}
+        goPrevWeek={goPrevWeek}
+        goCurrentWeek={goCurrentWeek}
+        onShowModal={showModalHandler}
+      />
+      <Modal showModal={showModal} onHideModal={hideModalHandler} />
       <Calendar weekDates={weekDates} />
     </>
   );
