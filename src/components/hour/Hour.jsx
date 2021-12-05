2a555c1ae661c7333501885f045ca2534bfe0743
annaKulyk <ankakulik1998@gmail.com>
2021-12-05 13:18:12 +0200

    fixed MinuteHand path

--- src/components/hour/Hour.jsx
+++ src/components/hour/Hour.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import MinuteHand from '../minuteHand/minuteHand';
+import MinuteHand from '../minuteHand/MinuteHand.jsx';
 import Event from '../event/Event';
 import { formatMins } from '../../utils/dateUtils.js';
 
